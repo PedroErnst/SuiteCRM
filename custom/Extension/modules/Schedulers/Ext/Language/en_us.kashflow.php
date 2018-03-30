b6aef0c4c308f68f747bbfbb7ae95f7b87825bac
michael <michael.crews@salesagility.com>
2018-03-30 12:15:14 +0100

    Add missing language file

---
+++ custom/Extension/modules/Schedulers/Ext/Language/en_us.kashflow.php
@@ -0,0 +1,4 @@
+<?php
+$mod_strings['LBL_GETCUSTOMERS'] = 'Kashflow: Get Customers';
+$mod_strings['LBL_GETINVOICES'] = 'Kashflow: Get Invoices';
+$mod_strings['LBL_GETPRODUCTS'] = 'Kashflow: Get Products';
